a621b24a1b9db7d48d1edd8e22d0c5f9413e2a76
FernandoTol <toledo.lufe@gmail.com>
2022-08-10 21:49:53 -0500

    add tarea 1

--- operadores.js
+++ operadores.js
@@ -1,15 +1,15 @@
 //generar un programa que te diga si un numero es mayor que otro
 
-let numUno = 1;
-let numDos = 2;
+let numUno      = 1;
+let numDos      = 2;
 let comparacion = false;
 
-if ( numUno >= numDos ){
+if ( numUno > numDos ){
 
     comparacion = true
     console.log(`El numero uno es mayor que numero dos: ${ comparacion }`)
 
-}else if( numUno <= numDos ){
+}else if( numUno < numDos ){
 
     comparacion = true
     console.log(`El numero dos es mayor que numero uno: ${ comparacion }`)
