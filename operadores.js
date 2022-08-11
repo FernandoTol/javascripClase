46ee34ad8d1f657e849df76c3f4db6e6287e76a5
FernandoTol <toledo.lufe@gmail.com>
2022-08-10 21:40:33 -0500

    add tarea 1

---
+++ operadores.js
@@ -0,0 +1,17 @@
+//generar un programa que te diga si un numero es mayor que otro
+
+let numUno = 1;
+let numDos = 2;
+let comparacion = false;
+
+if ( numUno >= numDos ){
+
+    comparacion = true
+    console.log(`El numero uno es mayor que numero dos: ${ comparacion }`)
+
+}else{
+
+    comparacion = true
+    console.log(`El numero dos es mayor que numero uno: ${ comparacion }`)
+
+}
---
+++ voriables.js
@@ -0,0 +1,1 @@
+let nombreMinusculas = 'fernando'
